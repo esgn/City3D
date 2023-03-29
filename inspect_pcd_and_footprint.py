8c4f43000a9f41872a7103ef2fe009b748c65a83
esgn <5435148+esgn@users.noreply.github.com>
2023-03-29 13:14:50 +0200

    small script to inspect failed reconstruction input data

---
+++ inspect_pcd_and_footprint.py
@@ -0,0 +1,24 @@
+import pyvista as pv    
+import os, sys
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--building_id", default="BATIMENT0000000320899175",
+                        help="Unique building id")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    pcd_file = "data/IGN/ply_extracts_shifted/"+args.building_id+".ply"
+    footprint_file = "data/IGN/obj_footprints_shifted/"+args.building_id+".obj"
+    plotter = pv.Plotter()
+    plotter.background_color = "white"
+    pcd = pv.read(pcd_file)
+    plotter.add_mesh(pcd, color='blue', smooth_shading=True)
+    footprint = pv.read(footprint_file)
+    plotter.add_mesh(footprint, color='red', smooth_shading=True)
+    plotter.show()
+
+if __name__ == "__main__":
+    main()
--- visualize_obj.py
+++
@@ -1,8 +0,0 @@
-import pyvista as pv    
-import os, sys
-
-# a possibly problematic result
-obj_file="data/IGN/results/BATIMENT0000000320899175.obj"
-
-mesh = pv.read(obj_file)
-mesh.plot()
